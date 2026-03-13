5c9b40b1f24fdde47b4baadffd5cd511d337598e
CeeJay1241 <alimceejay@gmail.com>
2026-03-13 18:10:31 -0500

    Working Project

---
+++ quiz_project/data.py
@@ -0,0 +1,12 @@
+question_data =[
+    {"type": "boolean", "difficulty": "easy", "category": "Science: Computers", "question": "RAM stands for Random Access Memory.", "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"type": "boolean", "difficulty": "easy", "category": "Science: Computers", "question": "Ada Lovelace is often considered the first computer programmer.", "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"type": "boolean", "difficulty": "easy", "category": "Science: Computers", "question": "The Windows 7 operating system has six main editions.", "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"type": "boolean", "difficulty": "easy", "category": "Science: Computers", "question": "The Python programming language gets its name from the British comedy group \"Monty Python.\"", "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"type": "boolean", "difficulty": "easy", "category": "Science: Computers", "question": "Time on computers is measured via the Epoch system.", "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"type": "boolean", "difficulty": "easy", "category": "Science: Computers", "question": "\"HTML\" stands for Hypertext Markup Language.", "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"type": "boolean", "difficulty": "easy", "category": "Science: Computers", "question": "Pointers were not used in the original C programming language; they were added later on in C++.", "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"type": "boolean", "difficulty": "easy", "category": "Science: Computers", "question": "In most programming languages, the operator ++ is equivalent to the statement \"+= 1\".", "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"type": "boolean", "difficulty": "easy", "category": "Science: Computers", "question": "The first IBM PC was released in 1981.", "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"type": "boolean", "difficulty": "easy", "category": "Science: Computers", "question": "Linus Torvalds created Linux and Git.", "correct_answer": "True", "incorrect_answers": ["False"]}
+]
---
+++ quiz_project/question_model.py
@@ -0,0 +1,8 @@
+class Question:
+    def __init__(self, question, answer):
+        """Store a question and its correct answer."""
+        self.text = question
+        self.answer = answer
+
+new_q = Question("The sky is blue", "True")
+
