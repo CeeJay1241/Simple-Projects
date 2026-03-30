279351224410be1342ffccf51fa55e32d003a2a5
Chijioke Alim <64105039+CeeJay1241@users.noreply.github.com>
2026-03-30 18:11:25 -0400

    Added Scoreboard

--- pong_game/main.py
+++ pong_game/main.py
@@ -1,6 +1,8 @@
 from turtle import Turtle, Screen
 from paddle import Paddle
-
+from ball import Ball
+import time
+from scoreboard import Scoreboard
 screen = Screen()
 
 screen.setup(width=800, height=600)
@@ -11,6 +13,10 @@ screen.tracer(0)
 l_paddle = Paddle((-350, 0))
 r_paddle = Paddle((350,0))
 
+
+scoreboard = Scoreboard()
+
+
 screen.listen()
 screen.onkey(r_paddle.go_up, 'Up')
 screen.onkey(l_paddle.go_up, "w")
@@ -18,9 +24,31 @@ screen.onkey(l_paddle.go_down, "s")
 screen.onkey(r_paddle.go_down, 'Down')
 screen.onclick(lambda x, y: screen.bye())
 
+ball = Ball()
+
 
 game_is_on = True
 while game_is_on:
+    time.sleep(0.06)
     screen.update()
+
+    #Detect collision with wall
+    ball.move()
+    if ball.ycor() > 270 or ball.ycor() < -270:
+        ball.bounce_y()
+
+    #Detect collision with paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    #Deterct right paddle miss  
+    if ball.xcor() > 370:
+        ball.reset_position()
+        scoreboard.l_point()
+
+    #Deterct left paddle miss
+    if ball.xcor() < -370:
+        ball.reset_position() 
+        scoreboard.r_point()
 
 screen.exitonclick()
