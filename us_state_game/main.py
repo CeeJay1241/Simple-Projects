d8cdf536f5384cd16c28e8870cd8bd048d538315
Chijioke Alim <64105039+CeeJay1241@users.noreply.github.com>
2026-05-11 23:50:45 -0500

    Add squirrel census data and fur color count script

--- us_state_game/main.py
+++ us_state_game/main.py
@@ -1,4 +1,39 @@
 import pandas
 
-data = pandas.read_csv("weather_data.csv")
-print(data["temp"])
+data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+# data_dict = data.to_dict()
+# #print(data_dict)
+
+# temp_list = data["temp"].to_list()
+# #print(temp_list)
+
+# average = sum(temp_list) / len(temp_list)
+
+# maximum = data["temp"].max()
+# monday = data[data.day == "Monday"]
+# monday_temp = monday.temp.values[0]
+
+# print(monday_temp)
+
+data_color = data["Primary Fur Color"].to_list()
+print(data_color)
+
+fur = {"Gray": [],
+       "Cinnamon": [],
+       "Black": []}
+
+g = 0
+c = 0
+b = 0
+for i in data_color:
+    if i == "Gray":
+        g += 1
+    elif i == "Cinnamon":
+        c += 1
+    elif i == "Black":
+        b += 1
+
+
+
+
+fur_color_count = pandas.DataFrame()
