7460f3e262a214e5bf7471589fc012e4d979810f
Chijioke Alim <64105039+CeeJay1241@users.noreply.github.com>
2026-05-12 00:11:26 -0500

    Simplify fur color counting using pandas boolean filtering

--- us_state_game/main.py
+++ us_state_game/main.py
@@ -1,40 +1,19 @@
 import pandas
 
 data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
-# data_dict = data.to_dict()
-# #print(data_dict)
 
-# temp_list = data["temp"].to_list()
-# #print(temp_list)
-
-# average = sum(temp_list) / len(temp_list)
-
-# maximum = data["temp"].max()
-# monday = data[data.day == "Monday"]
-# monday_temp = monday.temp.values[0]
-
-# print(monday_temp)
-
-data_color = data["Primary Fur Color"].to_list()
+gray_squirrels_count = len(data[data["Primary Fur Color"] == "Gray"])
+red_squirrels_count = len(data[data["Primary Fur Color"] == "Cinnamon"])
+black_squirrels_count = len(data[data["Primary Fur Color"] == "Black"])
 
 fur = {}
 
-g = 0
-c = 0
-b = 0
-for i in data_color:
-    if i == "Gray":
-        g += 1
-    elif i == "Cinnamon":
-        c += 1
-    elif i == "Black":
-        b += 1
 
-fur["Gray"] = [g]
-fur["Cinnamon"] = [c]
-fur["Black"] = [b]
+fur["Gray"] = [gray_squirrels_count]
+fur["Cinnamon"] = [red_squirrels_count]
+fur["Black"] = [black_squirrels_count]
 
-#print(fur)
 fur_color_count = pandas.DataFrame(fur)
 
 print(fur_color_count)
+
