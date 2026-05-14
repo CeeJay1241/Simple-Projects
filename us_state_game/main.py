c6f0c846fb5ae963b462f11adde34f2b3420e89c
Chijioke Alim <64105039+CeeJay1241@users.noreply.github.com>
2026-05-13 21:30:52 -0500

    Add state guessing game loop with CSV data and score tracking

--- us_state_game/main.py
+++ us_state_game/main.py
@@ -1,4 +1,5 @@
 import turtle
+import pandas
 
 screen = turtle.Screen()
 screen.title("U.S. States Game")
@@ -6,7 +7,21 @@ image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
-answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?")
-print(answer_state)
+num_chosen_states = 0
+
+data = pandas.read_csv("50_states.csv")
+data_list = data.state.to_list()
+data_list = [i.lower() for i in data_list]
+answer_state = screen.textinput(title="Guess the State", prompt=f"{num_chosen_states}/50 What's another state's name?")
+
+while num_chosen_states < 50:
+    if answer_state in data_list:
+        num_chosen_states +=1
+        data_list.remove(answer_state)
+        answer_state = screen.textinput(title="Guess the State", prompt=f"{num_chosen_states}/50What's another state's name?")
+    else:
+            answer_state = screen.textinput(title="Guess the State", prompt=f"{num_chosen_states}/50What's another state's name?")
+
+
 
 turtle.mainloop()
