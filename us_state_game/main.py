0edf9d6bcfa740d6f22959e9be0055c5a5aa1772
Chijioke Alim <64105039+CeeJay1241@users.noreply.github.com>
2026-05-13 23:33:35 -0500

    Add turtle drawing and state label placement on map

--- us_state_game/main.py
+++ us_state_game/main.py
@@ -7,21 +7,32 @@ image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
+turtle = turtle.Turtle()
+turtle.hideturtle()
+turtle.color("black")
+turtle.penup()
+
 num_chosen_states = 0
+def choose_state():
+    global answer_state
+    answer_state = (screen.textinput(title="Guess the State", prompt=f"{num_chosen_states}/50 What's another state's name?")).lower()
+    return answer_state
 
 data = pandas.read_csv("50_states.csv")
 data_list = data.state.to_list()
 data_list = [i.lower() for i in data_list]
-answer_state = screen.textinput(title="Guess the State", prompt=f"{num_chosen_states}/50 What's another state's name?")
+choose_state()
 
 while num_chosen_states < 50:
     if answer_state in data_list:
         num_chosen_states +=1
+        state_info = data[data.state == answer_state.capitalize()]
+        turtle.goto(x=int(state_info["x"].iloc[0]), y=int(state_info["y"].iloc[0]))
+        turtle.write(answer_state.capitalize(), align="center", font=("Arial", 8, "normal"))
+        print(state_info)
         data_list.remove(answer_state)
-        answer_state = screen.textinput(title="Guess the State", prompt=f"{num_chosen_states}/50What's another state's name?")
+        choose_state()
     else:
-            answer_state = screen.textinput(title="Guess the State", prompt=f"{num_chosen_states}/50What's another state's name?")
-
-
+        choose_state()
 
 turtle.mainloop()
