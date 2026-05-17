a0aee749646ab538edede9b126235335f4fdf4d6
Chijioke Alim <64105039+CeeJay1241@users.noreply.github.com>
2026-05-17 16:19:53 -0500

    Add start_timer function and format countdown as MM:SS

--- pomodoro/main.py
+++ pomodoro/main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import math
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -17,11 +18,16 @@ window.config(padx=100, pady=50, bg=YELLOW)
 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start_timer():
+    count_down(60 * WORK_MIN)
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 def count_down(count):
-    canvas.itemconfig(timer_text, text=count)
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
         window.after(1000, count_down, count-1)
 
@@ -37,9 +43,8 @@ canvas.create_image(100,112, image = tomato_img)
 timer_text = canvas.create_text(100,130, text="00:00", fill="white", font=(FONT_NAME,35, "bold"))
 canvas.grid(column=1, row=1)
 
-count_down(5)
 
-start_button = Button(text="Start", highlightthickness=0, padx=0, pady=0, relief="flat", bd=0)
+start_button = Button(text="Start", command=start_timer,  highlightthickness=0, padx=0, pady=0, relief="flat", bd=0)
 start_button.grid(column=0, row=2)
 
 reset_button = Button(text="Reset", bg=YELLOW, highlightthickness=0, padx=0, pady=0, relief="flat", bd=0)
